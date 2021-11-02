85779074f27904197ffd85424589af74ccee9f30
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-11-02 20:36:06 +0000

    stop ContentModel making contents collection

--- back-end/src/models/content.model.ts
+++ back-end/src/models/content.model.ts
@@ -1,5 +1,6 @@
-import { getModelForClass, prop } from '@typegoose/typegoose';
+import { getModelForClass, modelOptions, prop } from '@typegoose/typegoose';
 
+@modelOptions({ schemaOptions: { collection: 'content' } })
 export class Content {
   @prop({ required: true })
   title!: string;
