b26e9de0455b8b7b7cba173e7dddd14ab07b61cf
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-11-03 10:20:43 +0000

    create providers data

---
+++ back-end/db/data/test-data/providers.ts
@@ -0,0 +1,94 @@
+const providers = [
+  {
+    id: 'amazon',
+    name: 'Amazon',
+    url: 'https://www.amazon.com',
+  },
+  {
+    id: 'applebooks',
+    name: 'Apple Books',
+    url: 'https://books.apple.com/book-store',
+  },
+  {
+    id: 'applepodcasts',
+    name: 'Apple Podcasts',
+    url: 'https://podcasts.apple.com',
+  },
+  {
+    id: 'careerexplorer',
+    name: 'CareerExplorer',
+    url: 'https://www.careerexplorer.com/careers/',
+  },
+  {
+    id: 'coursera',
+    name: 'Coursera',
+    url: 'https://www.coursera.org',
+  },
+  {
+    id: 'edx',
+    name: 'edX',
+    url: 'https://www.edx.org',
+  },
+  {
+    id: 'fun-mooc',
+    name: 'Fun-Mooc',
+    url: 'https://www.fun-mooc.fr/fr/',
+  },
+  {
+    id: 'futurelearn',
+    name: 'FutureLearn',
+    url: 'https://www.futurelearn.com/',
+  },
+  {
+    id: 'getsmarter',
+    name: 'Getsmarter',
+    url: 'https://www.getsmarter.com',
+  },
+  {
+    id: 'linkedinlearning',
+    name: 'Linkedin Learning',
+    url: 'https://learning.linkedin.com',
+  },
+  {
+    id: 'medium',
+    name: 'Medium',
+    url: 'https://medium.com',
+  },
+  {
+    id: 'openclassrooms',
+    name: 'OpenClassrooms',
+    url: 'https://openclassrooms.com',
+  },
+  {
+    id: 'opensesame',
+    name: 'OpenSesame',
+    url: 'https://www.opensesame.com/',
+  },
+  {
+    id: 'opentextbooklibrary',
+    name: 'Open Textbook Library',
+    url: 'https://open.umn.edu/opentextbooks',
+  },
+  {
+    id: 'pluralsight',
+    name: 'Pluralsight',
+    url: 'https://www.pluralsight.com',
+  },
+  {
+    id: 'stackoverflow',
+    name: 'stackoverflow',
+    url: 'https://stackoverflow.com',
+  },
+  {
+    id: 'udemy',
+    name: 'Udemy',
+    url: 'https://www.udemy.com',
+  },
+  {
+    id: 'youtube',
+    name: 'YouTube',
+    url: 'https://www.youtube.com',
+  },
+];
+
+export default providers;
