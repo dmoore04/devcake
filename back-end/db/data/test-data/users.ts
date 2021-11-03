f917c370fc362762b1de2adc87443752f706ad1d
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-11-03 16:25:11 +0000

    add avatar urls to user model

--- back-end/db/data/test-data/users.ts
+++ back-end/db/data/test-data/users.ts
@@ -8,6 +8,8 @@ const users = [
     media: ['podcasts'],
     saved: ['617ffbea8bae4425133b1fe5'],
     username: 'j0hntr0n',
+    avatarUrl:
+      'https://metro.co.uk/wp-content/uploads/2020/05/PRI_151662478.jpg?quality=90&strip=all',
   },
   {
     name: 'Bread Manbread',
@@ -18,6 +20,7 @@ const users = [
     media: ['Podcasts'],
     saved: ['617ffbea8bae4425133b1fe5'],
     username: 'ManBre@d',
+    avatarUrl: 'https://www.melskitchencafe.com/wp-content/uploads/french-bread-roll1.jpg',
   },
   {
     name: 'Danny MOAR',
@@ -28,6 +31,8 @@ const users = [
     media: ['mooc, articles, books'],
     saved: ['617ffbea8bae4425133b1fe5'],
     username: 'D-man2k21',
+    avatarUrl:
+      'https://images.immediate.co.uk/production/volatile/sites/3/2019/08/Keanu-Reeves-5939c06.jpg?quality=90&resize=620%2C413',
   },
   {
     name: 'Alex Swine',
@@ -38,6 +43,8 @@ const users = [
     media: ['careers', 'distance learning'],
     saved: ['617ffbea8bae4425133b1fe5'],
     username: 'Sea-man01',
+    avatarUrl:
+      'https://www.royalnavy.mod.uk/-/media/careers-section-redesign/homepage/uniform-updates/7_whynavy_benefits_981x551.jpg?h=551&w=981&rev=9cdcc91ac1b140aeaad7b025bd7fa2b0&cropregion=0,0,0,0&hash=A5E49F6696BE1627101D511D083F51F4',
   },
 ];
 
--- back-end/src/models/user.model.ts
+++ back-end/src/models/user.model.ts
@@ -22,6 +22,9 @@ export class User {
 
   @prop({ ref: () => Content, default: [] })
   public saved?: Ref<Content>[];
+
+  @prop({ default: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png' })
+  public avatarUrl?: string;
 }
 
 export const UserModel = getModelForClass(User);
