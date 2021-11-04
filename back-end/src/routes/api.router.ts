6bd8ca17ead7eebc4420a81167eb5a50b6839d9d
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-11-04 10:36:49 +0000

    implement GET /api/topics route

--- back-end/src/routes/api.router.ts
+++ back-end/src/routes/api.router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import sendEndpoints from '../controllers/api.controllers';
+import topicsRouter from './topics.router';
 
 const apiRouter = Router();
 
+apiRouter.use('/topics', topicsRouter);
 apiRouter.get('/', sendEndpoints);
 
 export default apiRouter;
