019261e2e3c86f2b130fa3cc7dad3cf1f4eab622
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-11-04 11:03:08 +0000

    restructure db utils

--- back-end/__tests__/topics.spec.ts
+++ back-end/__tests__/topics.spec.ts
@@ -2,16 +2,9 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import config from 'config';
 import app from '../src/app';
-import { seedCollection } from '../src/utils/db';
-import { Topic, TopicModel } from '../src/models/topic.model';
-import { topics } from '../db/data/test-data';
 
 beforeAll(async () => {
   await mongoose.connect(config.get<string>('dbUri'));
-});
-
-beforeEach(async () => {
-  await seedCollection<Topic>(topics, TopicModel);
 });
 
 afterAll(async () => {
