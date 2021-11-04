d2854e8cc540466fe5ab51f9ef5480f4bd06ad56
Alexander Swain <alexander.swain03@gmail.com>
2021-11-04 14:06:58 +0000

    add GETmedia endpoint

--- back-end/src/routes/api.router.ts
+++ back-end/src/routes/api.router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import sendEndpoints from '../controllers/api.controllers';
 import topicsRouter from './topics.router';
+import mediaRouter from './media.router';
 
 const apiRouter = Router();
 
 apiRouter.use('/topics', topicsRouter);
+apiRouter.use('/media', mediaRouter);
 apiRouter.get('/', sendEndpoints);
 
 export default apiRouter;
