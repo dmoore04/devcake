ece635e652522d446ddc4e1399475269466f88be
Alexander Swain <alexander.swain03@gmail.com>
2021-11-04 14:37:37 +0000

    add GET content endpoint

--- back-end/src/routes/api.router.ts
+++ back-end/src/routes/api.router.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import sendEndpoints from '../controllers/api.controllers';
 import topicsRouter from './topics.router';
 import mediaRouter from './media.router';
+import contentRouter from './content.router';
 
 const apiRouter = Router();
 
 apiRouter.use('/topics', topicsRouter);
 apiRouter.use('/media', mediaRouter);
+apiRouter.use('/content', contentRouter);
 apiRouter.get('/', sendEndpoints);
 
 export default apiRouter;
