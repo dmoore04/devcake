49c9ae7569a24537945e4ca51046e186e6ff5071
matthewmanbrad <matthewmanbrad@gmail.com>
2021-11-05 10:46:36 +0000

    add nav to category choice and topic choice

--- front-end/src/pages/topicChoice.tsx
+++ front-end/src/pages/topicChoice.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import IPage from '../interfaces/page';
 import logging from '../config/logging';
 import { fetchTopics } from '../utils/api';
+import { Link } from 'react-router-dom';
 
 const TopicChoice: React.FC<IPage> = (props) => {
   const [topics, setTopics] = useState([]);
@@ -13,7 +14,17 @@ const TopicChoice: React.FC<IPage> = (props) => {
     });
   }, [props.name]);
 
-  return <p>This is the topic choice page!</p>;
+  return (
+    <div>
+      <p>This is the topic choice page!</p>
+      <Link key="home" to="/">
+        Home
+      </Link>
+      <Link key="category-choice" to="/category-choice">
+        Category Choice
+      </Link>
+    </div>
+  );
 };
 
 export default TopicChoice;
