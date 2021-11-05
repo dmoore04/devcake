d1e6883e95693c7e3f4d9484619de2912d737805
matthewmanbrad <matthewmanbrad@gmail.com>
2021-11-05 10:33:16 +0000

    sort nav bar

---
+++ front-end/src/components/nav-bar.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+
+const NavBar: React.FC = () => {
+  return (
+    <div>
+      <Link key="home" to="/">
+        Home
+      </Link>
+      <Link key="topic-choice" to="/topic-choice">
+        Topic Choice
+      </Link>
+      <Link key="category-choice" to="/category-choice">
+        Category Choice
+      </Link>
+      <Link key="user-profile" to="/user-profile">
+        User Profile
+      </Link>
+      <Link key="following" to="/user-profile">
+        Following
+      </Link>
+      <Link key="Bookmarks" to="/user-profile">
+        Bookmarks
+      </Link>
+    </div>
+  );
+};
+
+export default NavBar;
--- front-end/src/pages/home.tsx
+++ front-end/src/pages/home.tsx
@@ -1,12 +1,18 @@
 import React, { useEffect } from 'react';
 import IPage from '../interfaces/page';
 import logging from '../config/logging';
+import NavBar from '../components/nav-bar';
 
 const HomePage: React.FC<IPage> = (props) => {
   useEffect(() => {
     logging.info(`Loading ${props.name}`);
   }, [props.name]);
-  return <p>This is the homepage!</p>;
+  return (
+    <div>
+      <NavBar />
+      <p>This is the homepage!</p>
+    </div>
+  );
 };
 
 export default HomePage;
