2182d367279d8fac3bec262630b3f0339a4ec52e
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-11-06 11:00:49 +0000

    hash user passwords

--- back-end/src/models/user.model.ts
+++ back-end/src/models/user.model.ts
@@ -1,6 +1,16 @@
-import { getModelForClass, prop, Ref } from '@typegoose/typegoose';
+import { getModelForClass, prop, Ref, pre, DocumentType } from '@typegoose/typegoose';
 import { Content } from './content.model';
+import bcrypt from 'bcrypt';
+import { Document } from 'mongoose';
 
+@pre<User>('save', async function (next) {
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hashSync(this.password, salt);
+    this.password = hash;
+  }
+  return next();
+})
 export class User {
   @prop({ required: true })
   public name!: string;
@@ -25,6 +35,13 @@ export class User {
 
   @prop({ default: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png' })
   public avatarUrl?: string;
+
+  public static comparePassword(
+    this: DocumentType<User>,
+    candidatePassword: string
+  ): Promise<boolean> {
+    return bcrypt.compare(candidatePassword, this.password).catch((e) => false);
+  }
 }
 
 export const UserModel = getModelForClass(User);
