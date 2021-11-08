c35b4883660b545c4655279cf99e9fe5d0e42b7b
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-11-08 10:28:11 +0000

    implement POST /users/login endpoint

--- back-end/src/routes/user.router.ts
+++ back-end/src/routes/user.router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { sendUsers, postUser, patchUser } from '../controllers/users.controller';
+import { sendUsers, postUser, patchUser, loginUser } from '../controllers/users.controller';
 
 const usersRouter = Router();
 
 usersRouter.patch('/:user_id', patchUser);
+usersRouter.post('/login', loginUser);
 usersRouter.route('/').get(sendUsers).post(postUser);
 
 export default usersRouter;
