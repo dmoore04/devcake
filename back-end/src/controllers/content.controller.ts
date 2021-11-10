76d670d4dc6dd2cbd275d7fc0f5a26205a7a8bda
Dan Moore <56768881+dmoosey@users.noreply.github.com>
2021-11-10 10:23:20 +0000

    implement GET /content/:content_id

--- back-end/src/controllers/content.controller.ts
+++ back-end/src/controllers/content.controller.ts
@@ -1,11 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
-import findContent from '../services/content.service';
+import { findContent, findRelevantContent } from '../services/content.service';
 import mongoose from 'mongoose';
 
-const sendContent = async (req: Request, res: Response, next: NextFunction) => {
+export const sendContentFeed = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    let { page, limit, user_id } = req.query;
-    const content = await findContent(user_id, Number(page) || 1, Number(limit) || 10);
+    const { page, limit, user_id } = req.query;
+    const content = await findRelevantContent(user_id, Number(page) || 1, Number(limit) || 10);
+    res.status(200).send({ content });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const sendContent = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { content_id } = req.params;
+    const content = await findContent(content_id);
     res.status(200).send({ content });
   } catch (err) {
     next(err);
