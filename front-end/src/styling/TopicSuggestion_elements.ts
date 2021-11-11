dd901acb74fc264d8e511f31fe686c897a290179
matthewmanbrad <matthewmanbrad@gmail.com>
2021-11-11 10:44:20 +0000

    get right nav bar to stay to right of page

--- front-end/src/styling/TopicSuggestion_elements.ts
+++ front-end/src/styling/TopicSuggestion_elements.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 export const Suggestions = styled.section`
   width: 270px;
   float: right;
-  right: 0;
   background-color: #f9f2f2;
   padding-top: 2em;
   height: 100%;
