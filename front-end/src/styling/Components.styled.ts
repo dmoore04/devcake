0ca3db451641d0ada257d42a68d3c660d164b772
jatebo <john.tenbosch@gmail.com>
2021-11-11 10:42:11 +0000

    change styled component to div to stop react shouting

--- front-end/src/styling/Components.styled.ts
+++ front-end/src/styling/Components.styled.ts
@@ -28,7 +28,7 @@ export const Button = styled.button`
   color: white;
 `;
 
-export const SingleContentCard = styled.li`
+export const SingleContentCard = styled.div`
   box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
   margin: 5px;
   display: grid;
